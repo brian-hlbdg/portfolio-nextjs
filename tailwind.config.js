9011714ec68aa013a1d66af61aa6aa0f711aadb6
Brian Hall <brian.hlbdg@outlook.com>
2025-09-15 19:58:21 -0500

    fixed the tailwind issue, wrong version. Using there three version not the 4

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "rgb(211 84 0)",
+          dark: "rgb(180 70 0)",
+        },
+      },
+    },
+  },
+};
